e8a4163a3775617e8228aaab3454912d7a43fdfe
Marc Gavanier <marc.gavanier@protonmail.com>
2025-08-01 11:21:32 +0200

    feat: add sitemap generation

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,9 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'feature', 'production']).default('development'),
+  PROTOCOL: z.enum(['http', 'https']).default('https'),
+  HOSTNAME: z.string().min(1, { message: 'Hostname must not be empty' }),
+  PORT: z.coerce.number().int().min(1).max(65535).nullish(),
   NEXT_PUBLIC_APP_NAME: z.string().min(1, { message: 'App name must not be empty' }),
   NEXT_PUBLIC_BASE_PATH: z.string().refine((val) => val === '' || val.startsWith('/'), {
     message: "Must be empty or start with '/'"
