2a4018db99fd05c721e9740cf4e8a6c4af8f1469
Marc Gavanier <marc.gavanier@protonmail.com>
2025-09-03 09:46:21 +0200

    fix: temporary back to output export

--- src/app/(full-page)/(regions)/[region]/[departement]/lieux/[id]/page.tsx
+++ src/app/(full-page)/(regions)/[region]/[departement]/lieux/[id]/page.tsx
@@ -11,18 +11,24 @@ export const generateMetadata = async ({ params }: { params: Promise<{ id: strin
   };
 };
 
-const Page = async () => {
-  return (
-    <FicheLieuPage
-      breadcrumbsItems={[
-        { label: 'France', href: '/' },
-        { label: 'Bourgogne-Franche-Comté', href: '/bourgogne-franche-comte' },
-        { label: 'Territoire de Belfort (90)', href: '/bourgogne-franche-comte/territoire-de-belfort' },
-        { label: '90300 Éloie' }
-      ]}
-      listHref='/bourgogne-franche-comte/territoire-de-belfort'
-    />
-  );
-};
+export const generateStaticParams = () => [
+  {
+    region: 'bourgogne-franche-comte',
+    departement: 'territoire-de-belfort',
+    id: 'c9dff9b0-3c1f-4d2a-8c5e-6f7b8c9d0e3f'
+  }
+];
+
+const Page = async () => (
+  <FicheLieuPage
+    breadcrumbsItems={[
+      { label: 'France', href: '/' },
+      { label: 'Bourgogne-Franche-Comté', href: '/bourgogne-franche-comte' },
+      { label: 'Territoire de Belfort (90)', href: '/bourgogne-franche-comte/territoire-de-belfort' },
+      { label: '90300 Éloie' }
+    ]}
+    listHref='/bourgogne-franche-comte/territoire-de-belfort'
+  />
+);
 
 export default Page;
