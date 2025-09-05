ffa731c38c2ff7fadc668516e6f9f803358b7365
Marc Gavanier <marc.gavanier@protonmail.com>
2025-09-05 11:17:45 +0200

    ci: redeploy container for each image push

--- infrastructure/src/resources/image.ts
+++ infrastructure/src/resources/image.ts
@@ -4,6 +4,8 @@ import { config } from '../config';
 import { name } from '../utils/name';
 import { scalewayProvider } from './scaleway.provider';
 
+const commitSha = process.env.GITHUB_SHA?.substring(0, 8) ?? 'dev';
+
 const namespace = new registry.Namespace(
   name('registry-namespace'),
   {
@@ -14,16 +16,29 @@ const namespace = new registry.Namespace(
   { provider: scalewayProvider }
 );
 
-const image = new docker.Image(name('image'), {
-  build: {
-    context: '../',
-    dockerfile: '../Dockerfile',
-    platform: 'linux/amd64',
-    cacheFrom: {
-      images: [namespace.endpoint.apply((endpoint) => `${endpoint}/${config.projectSlug}:latest`)]
-    }
-  },
+const build = {
+  context: '../',
+  dockerfile: '../Dockerfile',
+  platform: 'linux/amd64',
+  cacheFrom: {
+    images: [namespace.endpoint.apply((endpoint) => `${endpoint}/${config.projectSlug}:latest`)]
+  }
+};
+
+new docker.Image(name('image-latest'), {
+  build,
   imageName: namespace.endpoint.apply((endpoint) => `${endpoint}/${config.projectSlug}:latest`),
+  skipPush: false,
+  registry: {
+    server: namespace.endpoint,
+    username: process.env.SCW_ACCESS_KEY,
+    password: process.env.SCW_SECRET_KEY
+  }
+});
+
+const image = new docker.Image(name('image-commit'), {
+  build,
+  imageName: namespace.endpoint.apply((endpoint) => `${endpoint}/${config.projectSlug}:${commitSha}`),
   skipPush: false,
   registry: {
     server: namespace.endpoint,
