96da8b1c406d3541251f582e2f1cd55c8732da99
Marc Gavanier <marc.gavanier@protonmail.com>
2025-10-28 10:12:19 +0100

    feat: responsive

---
+++ src/features/brand/use-cases/layout/navbar.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { Suspense } from 'react';
+import { RiMenuLine } from 'react-icons/ri';
+import { Geolocate } from '@/features/cartographie/geolocate';
+import { SearchAddress } from '@/features/cartographie/search-address';
+import { BesoinsFilters } from '@/features/lieux-inclusion-numerique/filters/besoins-filters';
+import { DisponibiliteFilters } from '@/features/lieux-inclusion-numerique/filters/disponibilite-filters';
+import { DispositifsFilters } from '@/features/lieux-inclusion-numerique/filters/dispositifs-filters';
+import { PublicCibleFilters } from '@/features/lieux-inclusion-numerique/filters/public-cible-filters';
+import { TerritoiresPrioritairesFilters } from '@/features/lieux-inclusion-numerique/filters/territoires-prioritaires-filters';
+import { CollapseController } from '@/libraries/ui/headless/collapse-controller';
+import { Button } from '@/libraries/ui/primitives/button';
+import { Link } from '@/libraries/ui/primitives/link';
+import { AppLogo } from '../logos';
+
+export const Navbar = () => (
+  <CollapseController>
+    {({ toggle, collapsible }) => (
+      <div className='px-8 py-4 shadow z-1'>
+        <div className='flex gap-2 justify-between'>
+          <Link
+            href='/'
+            title='Retour à l’accueil'
+            className='font-bold text-xl text-base-title flex items-center gap-2'
+            kind='link-hover'
+          >
+            <AppLogo />
+            Lieux d’inclusion numérique
+          </Link>
+          <Button kind='btn-link' className='px-2 lg:hidden' {...toggle}>
+            <RiMenuLine size={24} aria-hidden={true} />
+          </Button>
+        </div>
+        <div className='mt-3 flex gap-2 flex-col 2xl:flex-row justify-between'>
+          <div>
+            <div className='flex gap-2'>
+              <SearchAddress className='sm:w-100 w-full pr-0' />
+              <Geolocate />
+            </div>
+          </div>
+          <div
+            {...collapsible({
+              className: 'collapse lg:collapse-open w-fit'
+            })}
+          >
+            <div className='collapse-content py-1! pl-0! pr-1!'>
+              <Suspense fallback={<div>Chargement...</div>}>
+                <BesoinsFilters />
+                <PublicCibleFilters />
+                <DisponibiliteFilters />
+                <DispositifsFilters />
+                <TerritoiresPrioritairesFilters />
+              </Suspense>
+            </div>
+          </div>
+        </div>
+      </div>
+    )}
+  </CollapseController>
+);
