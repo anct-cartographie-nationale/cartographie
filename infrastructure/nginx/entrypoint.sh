1e3de6ce4ec45254e11757e9f6f371c6ec581d98
Marc Gavanier <marc@gavanier.com>
2026-04-23 12:48:04 +0200

    fix: hide nginx version, add error logs and graceful shutdown
    - server_tokens off: hide nginx version in response headers
    - error_log to stderr: errors visible in Grafana
    - entrypoint.sh: trap TERM/INT for graceful shutdown of both
      Nginx and Node.js on container scale-down

--- infrastructure/nginx/entrypoint.sh
+++ infrastructure/nginx/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
 node server.js &
+NODE_PID=$!
 
-exec nginx -g 'daemon off;'
+stop() {
+    nginx -s quit
+    kill "$NODE_PID"
+    wait "$NODE_PID"
+    exit 0
+}
+
+trap stop TERM INT
+
+nginx -g 'daemon off;' &
+wait $!
